b5e8c4b4f600fbb3c1def66420f1edcf6275ffe8
Victor IMORU <vimoru@pajuno.com>
2023-12-01 10:54:54 -0800

    update appSettings file

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AzWepApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greetings"] = configuration["Greetings"];
         }
     }
 }
